fb85eddb6036fd6b65fe1bfd316e3562e580b8fa
srivandhanaraju <30343269+srivandhanaraju@users.noreply.github.com>
2017-08-10 22:01:54 +0530

    Create Sum.c

---
+++ Sum.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+ int main() {
+    int num, temp, count=0;
+    printf("Enter an integer\n");
+    scanf("%d", &num);
+    temp = num;
+     while(temp!=0) {
+        temp = temp/10;
+        ++count;
+    }
+    printf("Number of digits in %d : %d",num, count);
+    return 0;
+    }
